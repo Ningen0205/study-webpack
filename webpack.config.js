1a697f222d2fcd45c72e06b2fe2020ede8a71cab
Harano <msg1801022@gmail.com>
2021-07-07 16:38:23 +0900

    init commit

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path')
+
+module.exports = {
+    mode: 'development',
+    entry: {app: './src/app.js' },
+    output: {
+        path: path.resolve(__dirname, 'public'),
+        filename: '[name].bundle.js'
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'postcss-loader',
+                    'sass-loader'
+                ]
+            },
+            {
+                // 画像及びwebfontのローダー処理
+                test: /\.(jpe?g|gif|png|svg|woff2?|ttf|eot)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: '[contenthash].[ext]',
+                            outputPath: 'images',
+                            publicPath: 'images'
+                        }
+                    }
+                ]
+            }
+        ]
+    }
+}
